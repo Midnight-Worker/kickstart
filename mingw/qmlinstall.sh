4bf4aed007f8ff6f20884ad32af5c26e3d70469b
Maik Tappe <maik.tappe82@googlemail.com>
2026-07-10 03:59:05 +0200

    ordner strukturier

---
+++ mingw/qmlinstall.sh
@@ -0,0 +1,1 @@
+pacman -S mingw-w64-x86_64-qt6-base mingw-w64-x86_64-qt6-declarative
---
+++ mingw/qt6install.sh
@@ -0,0 +1,6 @@
+pacman -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja
+
+pacman -S mingw-w64-x86_64-qt6
+pacman -S mingw-w64-x86_64-qt6-multimedia
+pacman -S mingw-w64-x86_64-qt6-quick3d
+pacman -S mingw-w64-x86_64-qt6-declarative 
